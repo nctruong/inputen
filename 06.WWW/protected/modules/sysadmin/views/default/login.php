1ba5e8f681fca2f159339bc8f41286f92639db80
nhminh.working <nhminh.working@gmail.com>
2012-08-19 11:52:57 +0000

    add theme yii

--- 06.WWW/protected/modules/sysadmin/views/default/login.php
+++ 06.WWW/protected/modules/sysadmin/views/default/login.php
@@ -1,82 +1,25 @@
-<div id="wrapper" class="login">		
-    <div id="right">
-        <div id="main">
-            <div class="section">
-                <div class="box">
-                    <div class="title">
-                        <h2>Sysadmin Cpanel</h2>
-                        <div class="hide"></div>
-                    </div>
-                    <div class="content nopadding">
-                        <div class="tabs">
-                            <div class="tabmenu">
-                                <ul> 
-                                    <li><a href="#tabs-1">Login</a></li> 
-                                    <!--                                    <li><a href="#tabs-2">Password forgotten?</a></li> -->
-                                </ul>
-                            </div>
-                            <div class="tab nopadding" id="tabs-1">
-                                <?php
-                                $form = $this->beginWidget('CActiveForm', array(
-                                    'id' => 'sysLoginForm',
-                                    'focus' => array($sysLoginForm, 'username'),
-                                    'enableAjaxValidation' => true,
-                                    'errorMessageCssClass' => 'alertBox-alert',
-                                        )
-                                );
-                                ?>
-<!--                                <div class="content">
-                                    <?php //echo $form->error($sysLoginForm, 'username', array('class' => 'system info')); ?>
-                                </div>-->
-                                <div class="row">
-                                    <?php echo $form->label($sysLoginForm, 'Username'); ?>
-                                    <div class="rowright"><?php echo $form->textField($sysLoginForm, 'username'); ?></div>
-                                </div>
-                                <div class="row">
-                                    <?php echo $form->label($sysLoginForm, 'Password'); ?>
-                                    <div class="rowright"><?php echo $form->passwordField($sysLoginForm, 'password'); ?></div>
-                                </div>
-                                <div class="row">
-                                    <label>
-                                        <div class="custom-checkbox">
-                                            <?php echo $form->checkBox($sysLoginForm, 'rememberMe', array('id' => 'remember')) ?>
-                                            <?php echo $form->label($sysLoginForm, 'Remember me?'); ?>
-                                        </div>
-                                    </label>
-                                    <div class="rowright button">
-                                        <?php //echo CHtml::submitButton('Login', array('type' => 'submit', 'class' => 'medium grey')); ?>
-                                        <button type="submit" class="medium grey"><span>Login</span></button>
-                                    </div>
-                                </div>
-                                <?php $this->endWidget(); ?>
-                            </div>
-
-                            <!--                            <div class="tab nopadding" id="tabs-2">
-                                                            <form action="">
-                                                                <div class="row">
-                                                                    <label>Username *</label>
-                                                                    <div class="rowright"><input type="text" value="" /></div>
-                                                                </div>
-                                                                <div class="row">
-                                                                    <label>Email Address *</label>
-                                                                    <div class="rowright"><input type="text" value="" /></div>
-                                                                </div>
-                                                                <div class="row">
-                                                                    <label>
-                                                                        * Required field
-                                                                    </label>
-                                                                    <div class="rowright button">
-                                                                        <button type="submit" class="medium grey"><span>Sumbit</span></button>
-                                                                    </div>
-                                                                </div>
-                                                            </form>
-                                                        </div>-->
-                        </div>
-
-                    </div>
-                </div>
-            </div>
-
+<div class="container" style="margin-top:100px;">
+    <div class="row-fluid">
+        <div class="span4">&nbsp;</div>
+        <div class="span3">
+            <?php
+            /** @var BootActiveForm $form */
+            $form = $this->beginWidget('bootstrap.widgets.TbActiveForm', array(
+                'id' => 'sysLoginForm',
+                'htmlOptions' => array('class' => 'well well-large'),
+                    ));
+            ?>
+            <h1>Login</h1>
+            <?php
+            echo $form->textFieldRow($sysLoginForm, 'username', array('class' => 'span10'));
+            echo $form->passwordFieldRow($sysLoginForm, 'password', array('class' => 'span10'));
+            echo $form->checkboxRow($sysLoginForm, 'rememberMe');
+            $this->widget('bootstrap.widgets.TbButton', array('buttonType' => 'submit', 'icon' => 'ok', 'label' => 'Submit'));
+            $this->endWidget();
+            ?>
         </div>
+        <div class="span4">&nbsp;</div>
     </div>
+
 </div>
+
--- 06.WWW/themes/sysadmin/views/layouts/login.php
+++ 06.WWW/themes/sysadmin/views/layouts/login.php
@@ -1,75 +1,38 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-
     <head>
-
         <meta http-equiv="Content-Type" content="text/html;charset=utf-8" /> 
         <title><?php echo CHtml::encode($this->pageTitle); ?></title>
-
-        <style type="text/css">
-            @import url("<?php echo Yii::app()->theme->getBaseUrl(); ?>/css/style.css");
-            @import url("<?php echo Yii::app()->theme->getBaseUrl(); ?>/css/style_text.css");
-            @import url("<?php echo Yii::app()->theme->getBaseUrl(); ?>/css/login.css");
-            @import url("<?php echo Yii::app()->theme->getBaseUrl(); ?>/css/link-buttons.css");
-            @import url("<?php echo Yii::app()->theme->getBaseUrl(); ?>/css/fullcalendar.css");
-            @import url("<?php echo Yii::app()->theme->getBaseUrl(); ?>/css/forms.css");
-            @import url("<?php echo Yii::app()->theme->getBaseUrl(); ?>/css/form-buttons.css");
-            @import url("<?php echo Yii::app()->theme->getBaseUrl(); ?>/css/accordion.css");
-            @import url("<?php echo Yii::app()->theme->getBaseUrl(); ?>/css/modalwindow.css");
-            @import url("<?php echo Yii::app()->theme->getBaseUrl(); ?>/css/system-messages.css");
-            @import url("<?php echo Yii::app()->theme->getBaseUrl(); ?>/css/datatable.css");
-            @import url("<?php echo Yii::app()->theme->getBaseUrl(); ?>/css/statics.css");
-            @import url("<?php echo Yii::app()->theme->getBaseUrl(); ?>/css/tabs.css");
-            @import url("<?php echo Yii::app()->theme->getBaseUrl(); ?>/css/alerts.css");
-            @import url("<?php echo Yii::app()->theme->getBaseUrl(); ?>/css/tooltip.css");
-            @import url("<?php echo Yii::app()->theme->getBaseUrl(); ?>/css/notifications.css");
-            @import url("<?php echo Yii::app()->theme->getBaseUrl(); ?>/css/prettify.css");
-            @import url("<?php echo Yii::app()->theme->getBaseUrl(); ?>/css/elfinder.css");
-            @import url("<?php echo Yii::app()->theme->getBaseUrl(); ?>/css/pirebox.css");
-            @import url("<?php echo Yii::app()->theme->getBaseUrl(); ?>/css/colorpicker.css");
-            @import url("<?php echo Yii::app()->theme->getBaseUrl(); ?>/css/wizard.css");
-            @import url("<?php echo Yii::app()->theme->getBaseUrl(); ?>/css/wysiwyg.css");
-            @import url("<?php echo Yii::app()->theme->getBaseUrl(); ?>/css/wysiwyg.modal.css");
-            @import url("<?php echo Yii::app()->theme->getBaseUrl(); ?>/css/wysiwyg-editor.css");
-        </style>
-
-        <!--[if lte IE 8]>
-                <script type="text/javascript" src="js/excanvas.min.js"></script>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content="administrator" />
+        <meta name="author" content="HMinh.IT" />
+        <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+        <!--[if lt IE 9]>
+          <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
         <![endif]-->
 
-        <script type="text/javascript" src="<?php echo Yii::app()->theme->getBaseUrl(); ?>/js/jquery-1.7.1.min.js"></script>
-        <script type="text/javascript" src="<?php echo Yii::app()->theme->getBaseUrl(); ?>/js/jquery-ui.js"></script>
-        <script type="text/javascript" src="<?php echo Yii::app()->theme->getBaseUrl(); ?>/js/jquery-ui-select.js"></script>
-        <script type="text/javascript" src="<?php echo Yii::app()->theme->getBaseUrl(); ?>/js/jquery-ui-spinner.js"></script>
-        <script type="text/javascript" src="<?php echo Yii::app()->theme->getBaseUrl(); ?>/js/jquery.customInput.js"></script>
-        <script type="text/javascript" src="<?php echo Yii::app()->theme->getBaseUrl(); ?>/js/jquery.dataTables.js"></script>
-        <script type="text/javascript" src="<?php echo Yii::app()->theme->getBaseUrl(); ?>/js/jquery.smartwizard-2.0.min.js"></script>
-        <script type="text/javascript" src="<?php echo Yii::app()->theme->getBaseUrl(); ?>/js/jquery.alerts.js"></script>
-        <script type="text/javascript" src="<?php echo Yii::app()->theme->getBaseUrl(); ?>/js/jquery.flot.js"></script>
-        <script type="text/javascript" src="<?php echo Yii::app()->theme->getBaseUrl(); ?>/js/jquery.graphtable-0.2.js"></script>
-        <script type="text/javascript" src="<?php echo Yii::app()->theme->getBaseUrl(); ?>/js/jquery.flot.pie.min.js"></script>
-        <script type="text/javascript" src="<?php echo Yii::app()->theme->getBaseUrl(); ?>/js/jquery.flot.resize.min.js"></script>
-        <script type="text/javascript" src="<?php echo Yii::app()->theme->getBaseUrl(); ?>/js/jquery.filestyle.mini.js"></script>
-        <script type="text/javascript" src="<?php echo Yii::app()->theme->getBaseUrl(); ?>/js/prettify.js"></script>
-        <script type="text/javascript" src="<?php echo Yii::app()->theme->getBaseUrl(); ?>/js/elfinder.min.js"></script>
-        <script type="text/javascript" src="<?php echo Yii::app()->theme->getBaseUrl(); ?>/js/jquery.jgrowl.js"></script>
-        <script type="text/javascript" src="<?php echo Yii::app()->theme->getBaseUrl(); ?>/js/colorpicker.js"></script>
-        <script type="text/javascript" src="<?php echo Yii::app()->theme->getBaseUrl(); ?>/js/jquery.tipsy.js"></script>
-        <script type="text/javascript" src="<?php echo Yii::app()->theme->getBaseUrl(); ?>/js/fullcalendar.min.js"></script>
-        <script type="text/javascript" src="<?php echo Yii::app()->theme->getBaseUrl(); ?>/js/pirobox.extended.min.js"></script>
-        <script type="text/javascript" src="<?php echo Yii::app()->theme->getBaseUrl(); ?>/js/jquery.validate.min.js"></script>
-        <script type="text/javascript" src="<?php echo Yii::app()->theme->getBaseUrl(); ?>/js/jquery.metadata.js"></script>
-        <script type="text/javascript" src="<?php echo Yii::app()->theme->getBaseUrl(); ?>/js/jquery.wysiwyg.js"></script>
-        <script type="text/javascript" src="<?php echo Yii::app()->theme->getBaseUrl(); ?>/js/controls/wysiwyg.image.js"></script>
-        <script type="text/javascript" src="<?php echo Yii::app()->theme->getBaseUrl(); ?>/js/controls/wysiwyg.link.js"></script>
-        <script type="text/javascript" src="<?php echo Yii::app()->theme->getBaseUrl(); ?>/js/controls/wysiwyg.table.js"></script>
-        <script type="text/javascript" src="<?php echo Yii::app()->theme->getBaseUrl(); ?>/js/plugins/wysiwyg.rmFormat.js"></script>
-        <script type="text/javascript" src="<?php echo Yii::app()->theme->getBaseUrl(); ?>/js/costum.js"></script>	
-
+        <!-- Le fav and touch icons -->
+        <link rel="shortcut icon" href="<?php echo Yii::app()->theme->getBaseUrl(); ?>/assets/ico/favicon.ico"/>
+        <link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php echo Yii::app()->theme->getBaseUrl(); ?>/assets/ico/apple-touch-icon-144-precomposed.png"/>
+        <link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php echo Yii::app()->theme->getBaseUrl(); ?>/assets/ico/apple-touch-icon-114-precomposed.png"/>
+        <link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php echo Yii::app()->theme->getBaseUrl(); ?>/assets/ico/apple-touch-icon-72-precomposed.png"/>
+        <link rel="apple-touch-icon-precomposed" href="<?php echo Yii::app()->theme->getBaseUrl(); ?>/assets/ico/apple-touch-icon-57-precomposed.png"/>
     </head>
+    <body>
+        <div class="navbar navbar-fixed-top">
+            <div class="navbar-inner">
+                <div class="container-fluid">
+                    <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </a>
+                    <a class="brand" href="/sysadmin.html"><?php echo Yii::app()->name; ?></a>
+                </div>
+            </div>
 
-    <body onload="prettyPrint()">
-        <?php echo $content; ?>
+            <div class="container-fluid">
+                <?php echo $content; ?>
+            </div> <!-- /container -->
     </body>
-
-</html> 
+</html>
