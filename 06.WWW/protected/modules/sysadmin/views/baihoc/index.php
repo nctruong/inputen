db97b30c48867651192ce7d9f6a810d5c9a1be16
nhminh.working <nhminh.working@gmail.com>
2012-08-25 07:44:40 +0000

    list/add/delete bai hoc

--- 06.WWW/protected/modules/sysadmin/views/baihoc/index.php
+++ 06.WWW/protected/modules/sysadmin/views/baihoc/index.php
@@ -1,21 +1,27 @@
 <?php
-
-$this->widget('bootstrap.widgets.TbGridView', array(
-    'type' => 'bordered ',
-    'dataProvider' => $dataProvider,
-    'selectableRows' => 2, // multiple rows can be selected
-    'template' => "{items}",
-    'columns' => array(
-        array(
-            'class' => 'CCheckBoxColumn',
-            'id' => 'cid',
-            'value' => '2',
-             'htmlOptions' => array('style' => 'width: 20px')
-        ),
-        array('name' => 'title', 'header' => 'Title'),
-        array('name' => 'slug', 'header' => 'Slug'),
-        array('name' => 'content', 'header' => 'Content'),
-        array('name' => 'created_date', 'header' => 'Created Date','htmlOptions' => array('style' => 'width: 150px')),
-    ),
-));
+$arrParams = array(
+    'modules' => Yii::app()->controller->module->id,
+    'controller' => Yii::app()->controller->id
+);
 ?>
+<form name="adminForm" id="adminForm" action="<?php echo Yii::app()->createUrl(MiisHelper::url($arrParams)) ?>" method="post">
+    <?php
+    $this->widget('bootstrap.widgets.TbGridView', array(
+        'type' => 'bordered ',
+        'dataProvider' => $dataProvider,
+        'selectableRows' => 2, // multiple rows can be selected
+        'template' => "{items}",
+        'columns' => array(
+            array(
+                'class' => 'CCheckBoxColumn',
+                'id' => 'cid',
+                'value' => '$data->id',
+                'htmlOptions' => array('style' => 'width: 20px')
+            ),
+            array('name' => 'title', 'header' => 'Title'),
+            array('name' => 'content', 'header' => 'Content'),
+            array('name' => 'created_date', 'header' => 'Created Date', 'htmlOptions' => array('style' => 'width: 150px')),
+        ),
+    ));
+    ?>
+</form>
