5fa9ab6b50d4f9cee7ae8957289b2766349f26bb
nhminh.working <nhminh.working@gmail.com>
2012-10-26 15:56:56 +0000

    ad session in Micontroller

--- 06.WWW/protected/components/MiisController.php
+++ 06.WWW/protected/components/MiisController.php
@@ -4,7 +4,14 @@ abstract class MiisController extends CController {
 
     public $layout = '//layouts/main';
 
+    public $_session;
+    
     public function beforeAction($action) {
+        
+        // create a sessio object
+        $this->_session = new CHttpSession;
+        $this->_session->open();
+        
         $theme = 'default';
         Yii::app()->setTheme(file_exists(dirname(__FILE__) . '/../../themes/' . $theme) ? $theme : 'default');
         Yii::app()->session['isLogin'] = 1;
